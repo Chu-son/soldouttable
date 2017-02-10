8767cd50aa001f9931d06fecdbaf3268377b1daf
Chu-son <s15mh133@mail.saitama-u.ac.jp>
2017-02-10 18:20:55 +0900

    up

--- soldouttable.py
+++ soldouttable.py
@@ -10,11 +10,131 @@ import json
 from getpass import getpass
 
 
+class DataContainers():
+    class Member():
+        def __init__(self, name):
+            self.name = name
+            self.schedule_list = []
+
+class Container():
+    def __init__(self, name):
+        self.name = name
+        self.data_list = []
+
 class DataContainer():
+    class ScheduleContainer():
+        class Table():
+            def __init__(self):
+                pass
+            def append_headline(self, headline):
+                pass
+            def new_member(self, name):
+                pass
+            def append_state(self, state):
+                pass
+
+        def __init__(self, schedule_name):
+            self.schedule_name = schedule_name
+            self.raw_table_list = []
+
+        def new_table(self):
+            self.raw_table_list.append(self.Table())
+
+        def new_member(self, 
+        def append_headline(self, headline):
+            self.raw_table_list[-1].append_headline(headline)
+
+        def append(self, row):
+            self.raw_table_list.append(row)
+
+        def organize_table(self):
+
+
+    def __init__(self, container_name):
+        self.container_name = container_name
+
+        self.schedules = []
+        self.infos = []
+
+        self.row_buf = []
+
+    def append_info(self, info):
+        self.infos.append(info)
+
+    def new_schedule(self, name):
+        self.schedules.append(self.ScheduleContainer(name))
+
+    def new_table(self):
+        self.schedules[-1].new_table()
+
+    def append_row_buffer(self, data):
+        self.row_buf.append(data)
+
+    def end_row(self):
+        self.schedules[-1].append(self.row_buf)
+        self.row_buf=[]
+
+    def print_schedules(self):
+        for schedule in self.schedules:
+            print(schedule.schedule_name)
+            for row in schedule.table_list:
+                for data in row:
+                    print(data, end=' ')
+                print()
+            print()
+
+    def _organize_datas(self):
+        self.max_cols = 0
+        self.max_rows = 0
+        self.member_list = []
+        self.header = []
+        for schedule in self.schedules:
+            self.max_rows = max(self.max_rows, len(schedule.table_list))
+            if len(schedule.table_list) != 0 :
+                self.max_cols = max(self.max_cols, len(schedule.table_list[0]))
+                self.header = schedule.table_list[0]
+
+                if len(self.member_list) == 0:
+                    for row in schedule.table_list:
+                        self.member_list.append(row[0])
+
+    def save_csv(self):
+        self._organize_datas()
+        with open(self.container_name + "_2.csv", "w") as f:
+            f.write(',')
+            for sch in self.schedules:
+                f.write(sch.schedule_name + ',')
+                f.write(','*(self.max_cols-2))
+            else:f.write('\n')
+            
+            header = ""
+            for h in self.header[1:]:
+                header += h+','
+            f.write(',' + header*len(self.schedules) + '\n')
+
+            for index in range(1, self.max_rows):
+                f.write(self.member_list[index] + ',')
+                for sch in self.schedules:
+                    if len(sch.table_list) == 0:
+                        f.write('-,'*(self.max_cols-1))
+                    else:
+                        for data in sch.table_list[index][1:]:
+                            f.write(data + ',')
+                f.write('\n')
+
+
+
+class __DataContainer():
     class ScheduleContainer():
         def __init__(self, schedule_name):
             self.schedule_name = schedule_name
             self.table_list = []
+
+            """
+            mamber
+            col_label
+            table_size
+            """
 
         def append(self, row):
             self.table_list.append(row)
@@ -140,19 +260,27 @@ class FortuneMusic():
         container = DataContainer(fname)
         with open(fname + ".csv","w") as f:
             pq = PQ(self.current_html)
+            # 個握販売状況抽出(会場ごと)
             contents = pq(".tab_content")[1:]
             for content in contents:
                 content = PQ(content)
+                # 日付と会場名取得
                 #print(PQ(content.find("font")[0]).text())
                 f.write(PQ(content.find("font")[0]).text() + ',\n')
                 container.append_schedule(PQ(content.find("font")[0]).text())
+
+                # 販売状況の表抽出
                 for table in content.find("table"):
+                    # 表中の行を抽出
                     for tr in PQ(table).find("tr"):
+                        # 行からヘッダ(列の見出し)抽出
                         headers = PQ(tr).find("th")
                         for header in headers:
                             #print(PQ(header).text(), end = "")
                             f.write(PQ(header).text() + ',')
                             container.append_row_buffer(PQ(header).text())
+
+                        # 行からデータ抽出
                         datas = PQ(tr).find("td")
                         for data in datas:
                             data = PQ(data)
@@ -160,6 +288,8 @@ class FortuneMusic():
                                 #print(data.text(), end = "")
                                 f.write(data.text() + ',')
                                 container.append_row_buffer(data.text())
+
+                            # 販売状況
                             if data.has_class("btn_area"):
                                 if len(data("select")) != 0:
                                     #print(" o ", end = "")
@@ -205,7 +335,7 @@ class FortuneMusic():
         urls = []
 
         eventblock = pq("#eventInfoArea")[0]
-        for block in PQ(eventblock)(".text")[1:]:
+        for block in PQ(eventblock)(".text")[0:]:
             block = PQ(block)('a')[0]
 
             url = PQ(block).attr("href")
@@ -254,6 +384,7 @@ def fortunemusic():
 
     # set default path
     fm.set_path()
+    #fm.print_current_html()
 
     # print exist event list
     for i, url in enumerate(fm._get_events()):
@@ -262,7 +393,7 @@ def fortunemusic():
 
     # save sale state for csv
     for url in fm._get_events():
-        if "keyaki" in url[0]:
+        if "nogizaka" in url[0]:
             path = fm._path_concatenation(fm.ROOT_PATH, url[0])
             fm.set_path(url = path)
             path = fm._get_koaku_page_path()
